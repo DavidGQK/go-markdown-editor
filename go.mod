13067eb0850296d6106ecc3b2138952f554e793d
David <davka8989@gmail.com>
2023-10-30 11:49:54 +0300

    initial working version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module fynemd
+
+go 1.20
+
+require fyne.io/fyne/v2 v2.4.1 // indirect
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+// config holds information that we can share with any function
+// that has a receiver of type *config
+type config struct {
+	EditWidget    *widget.Entry
+	PreviewWidget *widget.RichText
+	CurrentFile   fyne.URI
+	SaveMenuItem  *fyne.MenuItem
+}
+
+var cfg config
+
+func main() {
+	// create a fyne app
+	a := app.New()
+
+	// create a window for the app
+	win := a.NewWindow("Markdown")
+
+	// get the user interface
+	edit, preview := cfg.makeUI()
+	cfg.createMenuItems(win)
+
+	// set the content of the window
+	win.SetContent(container.NewHSplit(edit, preview))
+
+	// show window and run app
+	win.Resize(fyne.Size{Width: 800, Height: 500})
+	win.CenterOnScreen()
+	win.ShowAndRun()
+}
+
+// makeUI creates two widgets, assigns them to the app config, and
+// adds a listener on the edit widget that updates the preview widget
+// with parsed markdown whenever the user types something
+func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+	app.EditWidget = edit
+	app.PreviewWidget = preview
+
+	edit.OnChanged = preview.ParseMarkdown
+
+	return edit, preview
+}
+
+func (app *config) createMenuItems(win fyne.Window) {
+
+	// create three menu items
+	openMenuItem := fyne.NewMenuItem("Open...", func() {})
+	saveMenuItem := fyne.NewMenuItem("Save", func() {})
+	saveAsMenuItem := fyne.NewMenuItem("Save as...", func() {})
+
+	// create a file menu, and add the three items to it
+	fileMenu := fyne.NewMenu("File", openMenuItem, saveMenuItem, saveAsMenuItem)
+
+	// create a main menu, and add the file menu to it
+	menu := fyne.NewMainMenu(fileMenu)
+
+	// set the main menu for the application
+	win.SetMainMenu(menu)
+}
